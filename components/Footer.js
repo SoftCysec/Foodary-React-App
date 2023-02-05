a3e252bf76e66f1c06e8700d5664d803cbcbcf1b
SoftCysec <softengdedan@gmail.com>
2023-02-06 01:35:54 +0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( <
+        div >
+        <
+        p > Okware Foodary© 2023 < /p> <
+        /div>
+    );
+}
+
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,36 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+
+    },
+    recipe_2: {
+        name: 'Mashed Potatoes',
+        preparation_time: '1 hour',
+        servings: '5',
+        ingredients: [
+            '5 large potatoes, peeled and diced',
+            '1/4 cup butter',
+            '1/4 cup milk',
+            'Salt and pepper to taste',
+        ],
+        instructions: [
+            'Boil the diced potatoes in a large pot until they are tender',
+            'Drain the potatoes and return them to the pot',
+            'Add butter and milk to the pot and mash the potatoes until they are smooth',
+            'Season with salt and pepper to taste',
+        ],
+    },
+}
